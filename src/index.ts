3afeb3a8a2ca50fc40bb3d82df8122553e87faeb
clairefro <claire.froelich@gmail.com>
2021-05-02 14:12:59 -0700

    Add build script and config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 import { handleErrors } from "./middleware/handleErrors";
